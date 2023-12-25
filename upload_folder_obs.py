040c324c619eee5b619f7e5621c18cb858c7eba4
Mark Yan <xyzs996@gmail.com>
2023-12-25 16:15:56 +0800

    Update upload_folder_obs.py

--- upload_folder_obs.py
+++ upload_folder_obs.py
@@ -15,7 +15,7 @@ def main():
     sk = '6x5zWLdUtP6qNFCrK7A7177Er3v8ehAHsw8tIHnJ'
     obs_endpoint = 'obs.cn-north-4.myhuaweicloud.com'
     bucket_name = 'aitextvideopub'
-    local_folder_path = '/mnt/nvm2/aitexttovideo/漫画推文BGM'
+    local_folder_path = 'xxxxxxxxxxx' # 要上传的本地文件夹路径
     obs_folder_path = 'music/' # OBS中的目标文件夹路径
 
     obs_client = ObsClient(access_key_id=ak, secret_access_key=sk, server=obs_endpoint)
